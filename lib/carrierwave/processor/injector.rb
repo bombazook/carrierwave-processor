fc927bb0381895fee5da739b075e6f12e57b24b8
Alexander Kostrov <bombazook@gmail.com>
2014-03-24 08:03:21 +0400

    updated versions with saving root uploader

--- lib/carrierwave/processor/injector.rb
+++ lib/carrierwave/processor/injector.rb
@@ -3,9 +3,12 @@ require 'active_support/core_ext/class'
 
 class Injector < Module
 
+ # PLUGIN_OPTIONS = [:outer_version, :root_uploader]
+
   def initialize uploader, opts = {}, &block
     @uploader = uploader
     @outer_version = opts.delete(:outer_version)
+    @root_uploader = opts.delete(:root_uploader)
     @options = opts
     self.class_eval &block
     @uploader.prepend self
@@ -30,8 +33,9 @@ class Injector < Module
     version_options[:from_version] = @outer_version if @outer_version
     passing_options = {:if => ifs_array}
     passing_options[:outer_version] = args.first if args.first
+    passing_options[:root_uploader] = @root_uploader || @uploader
     version_args = version_options.empty? ? args : (args + [version_options])
-    @uploader.version *version_args do
+    passing_options[:root_uploader].version *version_args do
       Injector.new(self, passing_options, &block)
     end
   end
